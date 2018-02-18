11e8dc6c4724bcd7e024158cbfc77adb511577aa
Lef Ioannidis <rausted@gmail.com>
2018-02-18 14:39:03 -0500

    Use ByteString readFile

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,12 @@
 module Main where
+import Prelude hiding (readFile)
 import Control.Applicative
 import Data.Aeson
 import Data.Text (Text)
 import System.Environment
 import Schema
-
-main :: IO ()
-
-handleJsonFilename name = decode (readFile name) :: Maybe Value
+import Data.ByteString.Lazy (readFile)
 
 main = do
     args <- getArgs
-    mapM_ handleJsonFilename args
+    mapM_ (decode . readFile) args
