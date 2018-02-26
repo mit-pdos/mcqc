4351de3f86323c4acf4be4a3e772b7f6050c660f
Lef Ioannidis <elefthei@mit.edu>
2018-02-26 14:21:04 -0500

    Almost got constructors to work, need type resolution first

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -12,7 +12,12 @@ import Data.ByteString.Lazy.Char8 (ByteString, unpack, pack, concat)
 --     Declaration {what = "decl:ind", name = "tree", argnames = Just ["T"], typ = Nothing,
 --       constructors =
 --          Just [Constructor {what = Nothing, name = "Leaf", argtypes = Just [], argnames = Nothing},
---                Constructor {what = Nothing, name = "Node", argtypes = Just [Argtype {what = "type:glob", name = "tree", args = Just [Arg {what = "type:var", name = String "T"}]},Argtype {what = "type:var", name = "T", args = Nothing},Argtype {what = "type:glob", name = "tree", args = Just [Arg {what = "type:var", name = String "T"}]}], argnames = Nothing}]},
+--                Constructor {what = Nothing, name = "Node",
+-- 					argtypes =
+-- 						Just [Argtype {what = "type:glob", name = "tree", args = Just [Arg {what = "type:var", name = String "T"}]},
+--                            Argtype {what = "type:var", name = "T", args = Nothing},
+--                            Argtype {what = "type:glob", name = "tree", args = Just [Arg {what = "type:var", name = String "T"}]}],
+--                  argnames = Nothing}]},
 --
 --     Declaration {what = "decl:term", name = "get_left", argnames = Nothing, typ = Just (Typ {what = "type:arrow", left = Argtype {what = "type:glob", name = "tree", args = Just [Arg {what = "type:varidx", name = Number 1.0}]}, right = Argtype {what = "type:glob", name = "tree", args = Just [Arg {what = "type:varidx", name = Number 1.0}]}}), constructors = Nothing},Declaration {what = "decl:term", name = "get_val", argnames = Nothing, typ = Just (Typ {what = "type:arrow", left = Argtype {what = "type:glob", name = "tree", args = Just [Arg {what = "type:varidx", name = Number 1.0}]}, right = Argtype {what = "type:glob", name = "Datatypes.option", args = Just [Arg {what = "type:varidx", name = Number 1.0}]}}), constructors = Nothing}]}
 -- Pure makeModule function to be used as Functor
@@ -30,13 +35,13 @@ makeModule Module { what = s } = Left $ "Bad module 'what' key " ++ s
 -- TODO: Initialize constructors
 makeDecl :: Declaration -> Either String ByteString
 makeDecl Declaration { what = dt, name = dn, argnames = Just al, typ = Just tl, constructors = Just lc } =
-    Right $ concat ["Declaration ", (pack dn), " of type ", pack $ dt, " argnames: ", " types", " constructors: ", "\n"]
+    mconcat <$> mapM (makeCtor dn) lc
 makeDecl Declaration { what = dt, name = dn, argnames = Just al, typ = Nothing, constructors = Just lc } =
-    Right $ concat ["Declaration ", (pack dn), " of type ", pack $ dt, " argnames: ", " constructors: ",  "\n"]
+    mconcat <$> mapM (makeCtor dn) lc
 makeDecl Declaration { what = dt, name = dn, argnames = Nothing, typ = Just t1, constructors = Just lc } =
-    Right $ concat ["Declaration ", (pack dn), " of type ", pack $ dt, " types: ", " constructors: ", "\n"]
+    mconcat <$> mapM (makeCtor dn) lc
 makeDecl Declaration { what = dt, name = dn, argnames = Nothing, typ = Nothing, constructors = Just lc } =
-    Right $ concat ["Declaration ", (pack dn), " of type ", pack $ dt, " constructors", "\n"]
+    mconcat <$> mapM (makeCtor dn) lc
 makeDecl Declaration { what = dt, name = dn, argnames = Just al, typ = Just tl, constructors = Nothing } =
     Right $ concat ["Declaration ", (pack dn), " of type ", pack $ dt, " argnames: ", " types: ", "\n"]
 makeDecl Declaration { what = dt, name = dn, argnames = Just al, typ = Nothing, constructors = Nothing } =
@@ -44,27 +49,47 @@ makeDecl Declaration { what = dt, name = dn, argnames = Just al, typ = Nothing, 
 makeDecl Declaration { what = dt, name = dn, argnames = Nothing, typ = Just t1, constructors = Nothing } =
     Right $ concat ["Declaration ", (pack dn), " of type ", pack $ dt, " types: ", "\n"]
 makeDecl Declaration { what = dt, name = dn, argnames = Nothing, typ = Nothing, constructors = Nothing } =
-    Right $ concat ["Declaration ", (pack dn), " of type ", pack $ dt, "\n"]
-makeDecl d = Left $ unpack $ encode d
+    Right $ "" -- No constructors, arguments or types, nothing to return
 
 -- Coq to boogie constructors
 makeCtor :: String -> Constructor -> Either String ByteString
 makeCtor pname Constructor { what = Just ct, name = cn, argtypes = Just at, argnames = Just an } =
-    Right $ concat ["Constructor ", (pack cn), " of type ", (pack ct), " argtypes: ", " argnames: ", concat $ map pack an, "\n"]
+    Right $ concat [" +++ ", (pack cn), " of type ", (pack ct), " argnames: ", concat $ map pack an, " argtypes: ", concat $ map (debugToBytestring . makeArgtype) at,"\n"]
 makeCtor pname Constructor { what = Just ct, name = cn, argtypes = Just at, argnames = Nothing } =
-    Right $ concat ["Constructor ", (pack cn), " of type ", (pack ct), " argtypes: ", "\n"]
+    Right $ concat [" +++ ", (pack cn), " of type ", (pack ct), " argtypes: ",concat $ map (debugToBytestring . makeArgtype) at, "\n"]
 makeCtor pname Constructor { what = Just ct, name = cn, argtypes = Nothing, argnames = Just an } =
     Right $ concat ["Constructor ", (pack cn), " of type ", (pack ct), " argnames: ", concat $ map pack an, "\n"]
 makeCtor pname Constructor { what = Just ct, name = cn, argtypes = Nothing, argnames = Nothing } =
     Right $ concat ["Constructor ", (pack cn), " of type ", (pack ct), "\n"]
 makeCtor pname Constructor { what = Nothing, name = cn, argtypes = Just at, argnames = Just an } =
-    Right $ concat ["Constructor ", (pack cn), " argtypes: ", " argnames: ", concat $ map pack an, "\n"]
+    Right $ concat [" +++ ", (pack cn), " argtypes: ", concat $ map (debugToBytestring . makeArgtype) at, " argnames: ", concat $ map pack an, "\n"]
 makeCtor pname Constructor { what = Nothing, name = cn, argtypes = Just at, argnames = Nothing } =
-    Right $ concat ["Constructor ", (pack cn), " argtypes: ", "\n"]
+    Right $ concat ["function {:constructor} ", (pack . lowercase) cn, "`", pack (show $ length at), "(", concat $ map (debugToBytestring . makeArgtype) at, ")", "\n"]
 makeCtor pname Constructor { what = Nothing, name = cn, argtypes = Nothing, argnames = Just an } =
     Right $ concat ["Constructor ", (pack cn), " argnames: ", concat $ map pack an, "\n"]
 makeCtor pname Constructor { what = Nothing, name = cn, argtypes = Nothing, argnames = Nothing } =
     Right $ concat ["Constructor ", (pack cn), "\n"]
+
+debugToBytestring :: Either String ByteString -> ByteString
+debugToBytestring (Right bs) = concat $ [" {",bs, "} "]
+debugToBytestring (Left s) = concat $ ["! ", pack s, " !"]
+
+-- Argtype {what = "type:var", name = "T", args = Nothing},
+makeArgtype :: Argtype -> Either String ByteString
+makeArgtype Argtype { what = at, name = an, args = Just al } =
+-- function {:constructor} node`3(value:TT, left: Tree, right: Tree) : Tree;
+
+	Right $ concat $ [" name: ", pack $ an, " "] ++ map (debugToBytestring . makeArg) al
+makeArgtype Argtype { what = at, name = an, args = Nothing } = --value:TT
+    Right $ concat $ map pack [an, ":", at]
+
+makeArg :: Arg -> Either String ByteString
+makeArg Arg { what = at, name = String an} = Right $ concat $ map pack [show an, ":", at]
+makeArg Arg { what = at, name = Number an} = Right $ concat $ map pack [show an, ":", at]
+makeArg Arg { what = at, name = Bool an} = Right $ concat $ map pack [show an, ":", at]
+makeArg Arg { what = at, name = Object an} = Right $ concat $ map pack [show an, ":", at]
+makeArg Arg { what = at, name = Array an} = Right $ concat $ map pack [show an, ":", at]
+makeArg Arg { what = _, name = an} = Left $ "Bad argument: " ++ show an
 
 -- Parse JSON file into a Module
 parse :: ByteString -> Either String Module
--- src/Utils.hs
+++ src/Utils.hs
@@ -4,3 +4,6 @@ import Data.Char
 capitalised [] = []
 capitalised x = toUpper (head x) : map toLower (tail x)
 
+lowercase :: [Char] -> [Char]
+lowercase [] = []
+lowercase (h:ts) = (toLower h):(lowercase ts)
