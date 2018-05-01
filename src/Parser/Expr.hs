fcdf0ed0a797212d927baebb1c9cc7a015c33712
Lef Ioannidis <elefthei@mit.edu>
2018-04-30 20:47:31 -0400

    Catch quantifiers without 'what' field

--- src/Parser/Expr.hs
+++ src/Parser/Expr.hs
@@ -5,6 +5,7 @@ import Parser.Pattern
 import Data.Aeson
 import Data.Text
 import Data.HashMap.Strict
+import Prelude hiding (lookup)
 
 -- Cases
 data Case = Case { pat :: Pattern, body :: Expr}
@@ -19,13 +20,14 @@ data Typ =
 
 instance FromJSON Typ where
   parseJSON (Object v) =
-      case (v ! "what") of
-        "type:arrow" -> TypArrow <$> v .:  "left"
-                                 <*> v .:  "right"
-        "type:var"   -> TypVar   <$> v .:  "name"
-                                 <*> v .:? "args" .!= []
-        "type:glob"  -> TypGlob  <$> v .:  "name"
-        _ -> fail ("unknown kind: " ++ (show v))
+      case (lookup "what" v) of
+        Just "type:arrow"       -> TypArrow  <$> v .:  "left"
+                                             <*> v .:  "right"
+        Just "type:var"         -> TypVar    <$> v .:  "name"
+                                             <*> v .:? "args" .!= []
+        Just "type:glob"        -> TypGlob   <$> v .:  "name"
+        Just s                  -> fail ("unknown kind: " ++ (show v) ++ " because " ++ (show s))
+        Nothing                 -> fail ("No 'what' quantifier for: " ++ (show v))
 
 -- Expressions
 data Expr = ExprLambda { argnames :: [Text], body :: Expr }
@@ -38,15 +40,16 @@ data Expr = ExprLambda { argnames :: [Text], body :: Expr }
 
 instance FromJSON Expr where
   parseJSON (Object v) =
-      case (v ! "what") of
-        "expr:lambda"      -> ExprLambda      <$> v .:? "argnames" .!= []
-                                              <*> v .:  "body"
-        "expr:case"        -> ExprCase        <$> v .:  "expr"
-                                              <*> v .:  "cases"
-        "expr:constructor" -> ExprConstructor <$> v .:  "name"
-                                              <*> v .:? "args"     .!= []
-        "expr:apply"       -> ExprApply       <$> v .:  "func"
-                                              <*> v .:? "args"     .!= []
-        "expr:rel"         -> ExprRel         <$> v .:  "name"
-        "expr:global"      -> ExprGlobal      <$> v .:  "name"
-        s                  -> fail ("Unknown declaration type: " ++ (show v) ++ " because " ++ (show s))
+      case (lookup "what" v) of
+        Just "expr:lambda"      -> ExprLambda      <$> v .:? "argnames" .!= []
+                                                   <*> v .:  "body"
+        Just "expr:case"        -> ExprCase        <$> v .:  "expr"
+                                                   <*> v .:  "cases"
+        Just "expr:constructor" -> ExprConstructor <$> v .:  "name"
+                                                   <*> v .:? "args"     .!= []
+        Just "expr:apply"       -> ExprApply       <$> v .:  "func"
+                                                   <*> v .:? "args"     .!= []
+        Just "expr:rel"         -> ExprRel         <$> v .:  "name"
+        Just "expr:global"      -> ExprGlobal      <$> v .:  "name"
+        Just s                  -> fail ("Unknown expr: " ++ (show v) ++ " because " ++ (show s))
+        Nothing                 -> fail ("No 'what' quantifier for: " ++ (show v))
