7e124d88f1184b3278a0eda86b3a2242a482b22e
Lef Ioannidis <elefthei@mit.edu>
2018-06-07 15:42:36 -0400

    Lists cannot be constexpr

--- include/list.cpp
+++ include/list.cpp
@@ -6,12 +6,14 @@ namespace list {
 
     // Constructive cons, copies l so l can be referenced again
 	template<typename T>
-	constexpr inline static List<T> cons(List<T>& l, T t) {
-		return List<T>(l).push_front(t);
+	inline static List<T> cons(List<T>& l, T t) {
+		auto l2 = List<T>(l);
+		l2.push_front(t);
+		return l2;
     }
     // Destructive cons, reuses l so it can not be referenced again
 	template<typename T>
-	constexpr inline static List<T> dcons(List<T>& l, T t) {
+	inline static List<T> dcons(List<T>& l, T t) {
 		l.push_front(t);
 		return l;
     }
@@ -19,45 +21,45 @@ namespace list {
 	// Utility functions
     // List
     template<typename T>
-    constexpr inline static T head(List<T> l) {
+    inline static T head(List<T> l) {
 		return l.front();
     }
 
     // Constructive tail, l is considered immutable and will be copied safely
     template<typename T>
-    constexpr inline static List<T> tail(List<T> l) {
+    inline static List<T> tail(List<T> l) {
 		auto head = l.begin();
         return List<T>(head++, l.end());
 	}
     // Destructive tail, l is considered mutable and should not be referenced again as l
     template<typename T>
-    constexpr inline static List<T> dtail(List<T> l) {
+    inline static List<T> dtail(List<T> l) {
 		l.pop_front();
 		return l;
     }
 
     // Destructive app, both l1, l2 are mutable, l1 will be expanded, l2 will be deallocated. Neither should be referenced again.
     template<typename T>
-    constexpr inline static List<T> dappd(List<T> l1, List<T> l2) {
+    inline static List<T> dappd(List<T> l1, List<T> l2) {
 		l1.splice(l1.end(), l2);
 		delete l2;
 		return l1;
 	}
     // Left constructive app, both l1 mutable, l2 is immutable. l1 should not be referenced again.
     template<typename T>
-    constexpr inline static List<T> dapp(List<T> l1, List<T> l2) {
+    inline static List<T> dapp(List<T> l1, List<T> l2) {
 		l1.splice(l1.end(), l2);
 		return l1;
 	}
     // Right constructive app, both l1 is immutable, l2 is mutable. l2 should not be referenced again.
     template<typename T>
-    constexpr inline static List<T> appd(List<T> l1, List<T> l2) {
+    inline static List<T> appd(List<T> l1, List<T> l2) {
 		l2.splice(l2.begin(), l1);
 		return l2;
 	}
     // Fully constructive app, both l1, l2 are immutable and will be copied.
     template<typename T>
-    constexpr inline static List<T> app(List<T> l1, List<T> l2) {
+    inline static List<T> app(List<T> l1, List<T> l2) {
 		List<T> l3 = List<T>(l1);
 		l3.splice(l3.end(), l2);
 		return l3;
@@ -65,11 +67,11 @@ namespace list {
 
     // Boolean
 	template<typename T>
-    constexpr inline static bool empty(List<T> l) {
+    inline static bool empty(List<T> l) {
 		return l.empty();
 	}
     template<typename T>
-    constexpr inline static bool in(List<T> l, T t) {
+    inline static bool in(List<T> l, T t) {
 		for (auto it = l.begin(); it != l.end(); it++) {
 			if(*it == t)
 				return true;
@@ -78,7 +80,7 @@ namespace list {
 	}
     /// Arithmetic
 	template<typename T>
-	constexpr inline static nat::Nat length(List<T> l) {
+	inline static nat::Nat length(List<T> l) {
 		return static_cast<nat::Nat>(l.length());
 	}
 }
--- include/list.hpp
+++ include/list.hpp
@@ -16,7 +16,7 @@ namespace list {
 
 	// Destructive match, l is considered mutable and should not be referenced again as l
 	template<typename T>
-	constexpr const ListTuple<T> dmatch(List<T> l){
+	const ListTuple<T> dmatch(List<T> l){
 		switch(l.empty()) {
 		case true:  return {Nil, T(), List<T>()};
 		case false: return {Cons, l.front(), l.pop_front()};
@@ -25,7 +25,7 @@ namespace list {
 
 	// Constructive match, l is considered immutable and will be copied safely
 	template<typename T>
-	constexpr const ListTuple<T> match(List<T> l) {
+	const ListTuple<T> match(List<T> l) {
 		switch(l.empty()) {
 		case true:  return {Nil, T(), List<T>()};
 		case false: {
@@ -37,44 +37,44 @@ namespace list {
 
     // Constructive cons, copies l so l can be referenced again
 	template<typename T>
-	constexpr inline static List<T> cons(List<T>& l, T t);
+	inline static List<T> cons(List<T>& l, T t);
     // Destructive cons, reuses l so it can not be referenced again
 	template<typename T>
-	constexpr inline static List<T> dcons(List<T>& l, T t);
+	inline static List<T> dcons(List<T>& l, T t);
 
 	// Utility functions
     // List
     template<typename T>
-    constexpr inline static T head(List<T> l);
+    inline static T head(List<T> l);
 
     // Constructive tail, l is considered immutable and will be copied safely
     template<typename T>
-    constexpr inline static List<T> tail(List<T> l);
+    inline static List<T> tail(List<T> l);
     // Destructive tail, l is considered mutable and should not be referenced again as l
     template<typename T>
-    constexpr inline static List<T> dtail(List<T> l);
+    inline static List<T> dtail(List<T> l);
 
     // Destructive app, both l1, l2 are mutable, l1 will be expanded, l2 will be deallocated. Should not be referenced again.
     template<typename T>
-    constexpr inline static List<T> dappd(List<T> l1, List<T> l2);
+    inline static List<T> dappd(List<T> l1, List<T> l2);
     // Left constructive app, both l1 mutable, l2 is immutable. l1 should not be referenced again.
     template<typename T>
-    constexpr inline static List<T> dapp(List<T> l1, List<T> l2);
+    inline static List<T> dapp(List<T> l1, List<T> l2);
     // Right constructive app, both l1 is immutable, l2 is mutable. l2 should not be referenced again.
     template<typename T>
-    constexpr inline static List<T> appd(List<T> l1, List<T> l2);
+    inline static List<T> appd(List<T> l1, List<T> l2);
     // Fully constructive app, both l1, l2 are immutable and will be copied.
     template<typename T>
-    constexpr inline static List<T> app(List<T> l1, List<T> l2);
+    inline static List<T> app(List<T> l1, List<T> l2);
 
     // Boolean
 	template<typename T>
-    constexpr inline static bool empty(List<T> l);
+    inline static bool empty(List<T> l);
     template<typename T>
-    constexpr inline static bool in(List<T> l);
+    inline static bool in(List<T> l);
 
     /// Arithmetic
 	template<typename T>
-	constexpr inline static nat::Nat length(List<T> l);
+	inline static nat::Nat length(List<T> l);
 }
 #endif
