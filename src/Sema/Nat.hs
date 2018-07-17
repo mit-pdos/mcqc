e4c639dd39f02de0dc355c9702219fc9fef493af
Lef Ioannidis <elefthei@mit.edu>
2018-07-17 11:43:09 -0400

    Nat semantics work

--- src/Sema/Nat.hs
+++ src/Sema/Nat.hs
@@ -5,21 +5,18 @@ import Codegen.Expr
 import Codegen.Utils
 import Control.Lens
 
-natSemanticsCall :: CExpr -> CExpr
--- Semantics for O and S
-natSemanticsCall CExprCall { _fname = "Datatypes.O", _fparams = [] } = CExprNat 0
-natSemanticsCall CExprCall { _fname = "Datatypes.O", _fparams = [args] } = error "Datatypes.0 with args found!"
-natSemanticsCall CExprCall { _fname = "Datatypes.S", _fparams = [a] } = CExprNat $ (_nat . natSemanticsCall $ a) + 1
-natSemanticsCall CExprCall { _fname = "Datatypes.S", _fparams = a:arg } = error "Datatypes.S with more than one args found!"
-natSemanticsCall other = other
-
--- Semantics for higher order expr
 natSemantics :: CExpr -> CExpr
-natSemantics = over lbody natSemanticsCall
-                  . over (items . traverse) natSemanticsCall
-                  . over cexpr natSemanticsCall
-                  . over (cases . traverse) natSemanticsCall
-                  . over mpat natSemanticsCall
-                  . over mbody natSemanticsCall
-                  . over (fparams . traverse) natSemanticsCall
-                  . over (items . traverse) natSemanticsCall
+-- Semantics for O and S
+natSemantics CExprCall { _fname = "Datatypes.O", _fparams = [] } = CExprNat 0
+natSemantics CExprCall { _fname = "Datatypes.O", _fparams = [args] } = error "Datatypes.0 with args found!"
+natSemantics CExprCall { _fname = "Datatypes.S", _fparams = [a] } = CExprNat $ (_nat . natSemantics $ a) + 1
+natSemantics CExprCall { _fname = "Datatypes.S", _fparams = a:arg } = error "Datatypes.S with more than one args found!"
+-- Propagate to children expr
+natSemantics CExprLambda { .. } = CExprLambda _largs (natSemantics _lbody)
+natSemantics CExprCase   { .. } = CExprCase (natSemantics _cexpr) (map natSemantics _cases)
+natSemantics CExprMatch  { .. } = CExprMatch (natSemantics _mpat) (natSemantics _mbody)
+natSemantics CExprCall   { .. } = CExprCall _fname (map natSemantics _fparams)
+natSemantics CExprTuple  { .. } = CExprTuple (map natSemantics _items)
+-- Transparent to the rest of expressions
+natSemantics other = other
+
