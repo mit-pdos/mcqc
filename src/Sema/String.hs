de1473c1dd684f0782137db0eadab0a0c6392113
Lef Ioannidis <elefthei@mit.edu>
2019-01-11 16:31:41 -0500

    String semantics

--- src/Sema/String.hs
+++ src/Sema/String.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE RecordWildCards, OverloadedStrings  #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Sema.String where
 import CIR.Expr
 import Common.Utils
 import Data.MonoTraversable
+import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Bits
+import Debug.Trace
 
 -- Ascii as byte semantics
 asciiSemantics :: CExpr -> CExpr
@@ -16,17 +19,24 @@ asciiSemantics CExprCall { _cd = CDef { _nm = "Ascii.Ascii" }, _cparams = fp }
           makeByte (b:bs) = fromBool b `shift` (7 - length bs) .|. makeByte bs
 asciiSemantics other = omap asciiSemantics other
 
+-- Parse strings
+serialize :: CExpr -> (Maybe Text, Maybe CExpr)
+serialize CExprCall { _cd = CDef { _nm = "String" }, _cparams = [s@CExprStr { .. }, e] } =
+    case serialize e of
+      (Just estr, Nothing) -> (Just $ _str `T.append` estr, Nothing)
+      (Just estr, Just e)  -> (Just $ _str `T.append` estr, Just $ stringSemantics e)
+      (Nothing, Just e)    -> (Nothing, Just $ stringSemantics e)
+      (Nothing, Nothing)   -> (Nothing, Just $ stringSemantics e)
+serialize CExprCall { _cd = CDef { _nm = "EmptyString" } } = (Just "", Nothing)
+serialize e = (Nothing, Just e)
+
 -- Handle string semantics
 stringSemantics :: CExpr -> CExpr
-stringSemantics CExprCall { _cd = CDef { _nm = "String" }, _cparams = [char, str] } =
-    case (char, str) of
-        (CExprStr { _str = h }, CExprStr { _str = tl }) -> CExprStr $ h `T.append` tl
-        (CExprStr { _str = h }, CExprCall { _cd = CDef { _nm = "EmptyString" }, .. }) -> CExprStr h
-        (_, _) -> CExprCall (CDef "mkstring" strT) [char, str]
-    where strT = CTBase "string"
-stringSemantics CExprCall { _cd = CDef { _nm = "EmptyString" }, _cparams = [] } =
-    CExprStr mempty
-stringSemantics CExprCall { _cd = CDef { _nm = "EmptyString" }, .. } =
-    error "EmptyString takes no arguments"
-stringSemantics other = omap stringSemantics other
+stringSemantics e | trace ("String Semantics for  " ++ show e) False = undefined
+stringSemantics e =
+    case serialize e of
+      (Just str, Nothing) -> CExprStr str
+      (Just str, Just e)  -> CExprCall (CDef "append" (CTBase "string")) [CExprStr str, omap stringSemantics e]
+      (Nothing, Just e)   -> omap stringSemantics e
+      (Nothing, Nothing)  -> omap stringSemantics e
 
