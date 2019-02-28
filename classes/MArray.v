e931573a925d93080a8bf81f8f995cfb72521f69
Lef Ioannidis <elefthei@mit.edu>
2019-02-27 22:16:12 -0500

    Keep monadic arrays

--- classes/MArray.v
+++ classes/MArray.v
@@ -1,45 +1,25 @@
-(** Matches include/List.hpp *)
+(** Matches include/Array.hpp *)
 
 Set Implicit Arguments.
 
+Require MProc.
+Import MProc.Proc.
+
+
 Module Array.
 
-  Inductive array: Type -> Type :=
-    | get: forall T, array T -> nat -> array T
-    | put: forall T, array T -> nat -> T -> array T
-    | empty: forall T,  nat -> T -> array T
+  Inductive Array: Type -> Type :=
+    | get: forall T, Array T -> nat -> Array T
+    | put: forall T, Array T -> nat -> T -> Array T
+    | empty: forall T,  nat -> T -> Array T
     (** Monad implementation *)
-    | ret: forall T, T -> array T
-    | bind: forall T T', array T -> (T -> array T') -> array T'.
-
-  Class NativeArray A List :=
-  {
-    hget:    nat -> List -> A;
-    hput:    nat -> A -> List -> List;
-    hempty:  nat -> A -> List;
-  }.
-
-  Parameter Array: Type -> Type.
-
-  Instance nativeArray {T}: NativeArray T (Array T). Admitted.                                   
-
-  (** Define some arrays *)
-  Definition m := let zero := empty 4 0 in put zero 2 1.
-  Definition h := let zero := hempty 4 0 in hput 2 1 zero.
-
-  Compute m.
-  Compute h.
-
-  Theorem equalityBySameness: h = h.
-    Proof.
-      unfold h.
-      assert(m = m). unfold m.
-      reflexivity. reflexivity.
-    Qed.
+    | ret: forall T, T -> Array T
+    | bind: forall T T', Array T -> (T -> Array T') -> Array T'.
+  
+  Parameter runArray: forall T,  Array T -> proc T.
+  
+  (** Define some Arrays *)
+  Compute let zero := empty 4 0 in put zero 2 1.
 
 End Array.
-
-Require Extraction.
-Extraction Language JSON.
-Separate Extraction Array.m Array.h. 
 
